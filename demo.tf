592317ebc3ea37785f8fa97cb91d50f5f420b18a
Sanjeev <aclouddba@gmail.com>
2021-12-05 19:59:18 +0000

    initial commit

---
+++ demo.tf
@@ -0,0 +1,31 @@
+variable "compartment_ocid" {
+    default = "ocid1.tenancy.oc1..aaaaaaaaqg3jfw7lpittprwjto6ocbkwf5vopxstw3ifnm7lhvg7bomuj7iq"
+}
+variable "region" {
+    default = "us-ashburn-1"
+}
+
+variable "bucket_namespace" {
+    default = "saasops"
+}
+variable "bucket_name" {
+    default = "resource_manager_demo_bucket"
+}
+
+provider "oci" {
+    region = var.region
+}
+
+resource "oci_objectstorage_bucket" "create_bucket" {
+    # required
+    compartment_id = var.compartment_ocid
+    name = var.bucket_name
+    namespace = var.bucket_namespace
+
+    # optional
+    access_type = "NoPublicAccess" # <---- updated from "ObjectRead"
+}
+
+output "new_bucket" {
+    value = oci_objectstorage_bucket.create_bucket
+}
