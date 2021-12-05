e118d08574689049f830f6524e85c3042bff7c27
Sanjeev <aclouddba@gmail.com>
2021-12-05 20:24:21 +0000

    Fixed namespace in demo.tf

--- demo.tf
+++ demo.tf
@@ -6,7 +6,7 @@ variable "region" {
 }
 
 variable "bucket_namespace" {
-    default = "devopsdba"
+    default = "idkdrwtqztt3"
 }
 variable "bucket_name" {
     default = "resource_manager_demo_bucket"
@@ -20,7 +20,7 @@ resource "oci_objectstorage_bucket" "create_bucket" {
     # required
     compartment_id = var.compartment_ocid
     name = var.bucket_name
-    #namespace = var.bucket_namespace
+    namespace = var.bucket_namespace
 
     # optional
     access_type = "NoPublicAccess" # <---- updated from "ObjectRead"
